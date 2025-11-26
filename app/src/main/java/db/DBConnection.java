cd63b9bc222b561b84183e8800e289f7124fb0e0
lilohdong <rhkdrh102010@gmail.com>
2025-11-27 00:02:16 +0900

    Connection이 싱글톤이라 db가 계속 닫히는 문제가 생겨 매번 생성, 삭제하는 개념으로 변경, getConnection으로 사용하는 것은 똑같음

--- app/src/main/java/db/DBConnection.java
+++ app/src/main/java/db/DBConnection.java
@@ -7,8 +7,7 @@ public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/POS_PLACE";
     private static final String USER = "root";
     private static final String PASSWORD = "1234";
-
-    private static Connection conn;
+    
 
     public static Connection getConnection() {
         if (conn == null) {
