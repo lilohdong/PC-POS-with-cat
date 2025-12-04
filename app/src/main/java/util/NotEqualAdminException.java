90808ac10dd104870172c051c29c62442ec996df
lilohdong <rhkdrh102010@gmail.com>
2025-12-05 01:25:53 +0900

    예외를 Exception 상속으로 변경

--- app/src/main/java/util/NotEqualAdminException.java
+++ app/src/main/java/util/NotEqualAdminException.java
@@ -1,6 +1,6 @@
 package util;
 // Admin 아닐 떄 발생시키는 메소드 RuntimeException 계열 예외
-public class NotEqualAdminException extends RuntimeException {
+public class NotEqualAdminException extends Exception {
     public NotEqualAdminException(String message) {
         super(message);
     }
